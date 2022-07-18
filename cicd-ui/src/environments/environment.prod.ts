c2297f310c3cd6632ed7f7ce1715854c51823d7b
Sheshanath <35036388+sheshanathkumar@users.noreply.github.com>
2022-07-18 13:36:28 +0530

    updated environment

--- cicd-ui/src/environments/environment.prod.ts
+++ cicd-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api_url: 'http://cicdapp-env.eba-3qw9smzs.ap-south-1.elasticbeanstalk.com',
+  api_url: 'http://cicd-test.ap-south-1.elasticbeanstalk.com',
   production: true
 };
